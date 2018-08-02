701fe6ffc7b1b863b929ec208a68719db13d16bf
Alexsia Light <elightl729@comcast.net>
2018-08-02 12:44:32 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -1,11 +1,15 @@
 require_relative 'config/environment'
-
+require_relative 'models/model.rb'
 class App < Sinatra::Base
 
-  get '/food_form' do
+  get '/' do
     erb :food_form
   end
 
   # Add your post route and action below
+ post '/' do
+   @my_results = get_spooked(params[:question1])
+   erb :results
+  end
 
-end
+end 
---
+++ models/model.rb
@@ -0,0 +1,8 @@
+def get_spooked(door)
+  if door == "doorthree"
+    return "boo! ya got SPOOKED"
+  else 
+    return "it's okay girl u safe"
+  end
+  
+end
